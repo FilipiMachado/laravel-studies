46066563df9c8c436543ec664a5eceaca9b8e8bd
fil <filipisee@hotmail.com>
2022-11-26 15:49:41 -0300

    reworked base project

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,17 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
-
 Route::get('/about', function () {
     return view('about');
 });
-
-/* Route::get('/contact', function () {
-return view('contact');
-}); */
-
-//Route::get('/contact', 'ContactController@index');
-Route::get('/contact', [ContactController::class, 'index']);
