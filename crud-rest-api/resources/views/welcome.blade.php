070c68f559abf1b62875560ffb1d42c96c430be0
fil <filipisee@hotmail.com>
2022-11-29 21:56:30 -0300

    tests

--- crud-rest-api/resources/views/welcome.blade.php
+++ crud-rest-api/resources/views/welcome.blade.php
@@ -46,6 +46,9 @@
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <div>
+                        <h1>TEST</h1>
+                    </div>
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
