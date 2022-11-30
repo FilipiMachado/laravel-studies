cb6d7c79bc4138b616192fd214524f75ff293560
fil <filipisee@hotmail.com>
2022-11-29 21:54:24 -0300

    added controller

--- crud-rest-api/routes/api.php
+++ crud-rest-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SclassController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::get('/class', [SclassController::class, 'Index']);
